1e69f8e373dcc76a310cf7e52cb5c09c28a4059f
fabianobarroli <fabianobarroli@gmail.com>
2022-03-18 13:47:45 -0300

    criando sigint e exportando bdm

---
+++ src/database/db-sigint-export.js
@@ -0,0 +1,15 @@
+import sqlite3 from 'sqlite3'
+import {dirname} from 'path'
+import { fileURLToPath } from 'url'
+sqlite3.verbose()
+const filePath = dirname(fileURLToPath(import.meta.url)) + '/bancodedadosmatricula.db'
+const bdm = new sqlite3.Database(filePath);
+
+process.on('SIGINT', ()=> 
+    bdm.close(()=>{
+        console.log('BDM fechado!');
+        process.exit(0);
+    })
+);
+
+export default bdm
