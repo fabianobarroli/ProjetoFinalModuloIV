2a985f5d70956141af62ea885f494a41ac8fe49c
fabianobarroli <fabianobarroli@gmail.com>
2022-03-22 13:54:24 -0300

    instalando cors

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ import matriculaController from "./controllers/matricula-controller.js";
 import generalMiddeleware from "./middleware/genera-middleware";
 import bancodedadosmatricula from "./database/db-sigint-export.js";
 import bdm from "./database/db-sigint-export.js";
+import cors from 'cors';
+
+app.use(cors())
 const app = express()
 app.use(express.json())
 generalMiddeleware(app)
