2dc8a1b390c19d658b6c1379a039ec4245d24654
fabianobarroli <fabianobarroli@gmail.com>
2022-03-23 19:36:42 -0300

    criando os primeiros usuários do bdm

--- src/database/create-bdm-populate.js
+++ src/database/create-bdm-populate.js
@@ -29,9 +29,14 @@ INSERT INTO MATRICULA (ID, NOME_DO_ALUNO, NOME_DO_PAI, NOME_DA_MAE,
     CPF_ALUNO, CARTEIRA_DE_IDENTIDADE, MATRICULA_CERTIDAO_DE_NAS_OU_CAS,
      ENDERECO, TELEFONE_RESIDENCIAL, CELULAR, TELEFONE_PAI, TELEFONE_MAE, EMAIL_ESTUDANTE, EMAIL_RESPONSAVEL)
 VALUES
-(1, )
-(2, )
-(3, )
+(1,'Pedro de Alcântara', 'Pedro de Alcântara Bragança', 'Maria Leopoldina da Áustria', '111.222.333-99', '123456789', '21912601552017135680035247553542',
+'Palácio Imperial de São Cristóvão, Rio de Janeiro, RJ - Brasil', '2134915708', '31999992121', '21988774455', '21988774456', 'pedrinho@imperiodobrasil.br', 'pedroi@imperiodobrasil.br'),
+(2, 'João Carlos', 'Pedro de Alcântara Bragança', 'Maria Leopoldina da Áustria', '111.111.333-99', '11155588', '1820260155201313861950151452021',
+'Palácio Imperial de São Cristóvão, Rio de Janeiro, RJ - Brasil', '2134915708', '31999991121', '21988774455', '21988774456', 'joaocarlos@imperiodobrasil.br', 'pedroi@imperiodobrasil.br'),
+(3, 'Januária do Brasil', 'Pedro de Alcântara Bragança', 'Maria Leopoldina da Áustria', '555.111.333-99', '17155588', '11819501552010147896924636797131',
+'Palácio Imperial de São Cristóvão, Rio de Janeiro, RJ - Brasil', '2134915708', '31999991101', '21988774455', '21988774456', 'januaria@imperiodobrasil.br', 'pedroi@imperiodobrasil.br'),
+(4, 'Paula do Brasil', 'Pedro de Alcântara Bragança', 'Maria Leopoldina da Áustria', '558.111.333-99', '17155688', '27595401552020184621741544915712',
+'Palácio Imperial de São Cristóvão, Rio de Janeiro, RJ - Brasil', '2134915708', '31999991102', '21988774455', '21988774456', 'paula@imperiodobrasil.br', 'pedroi@imperiodobrasil.br'),
 `
 
 function criarTabelaMatricula(){
