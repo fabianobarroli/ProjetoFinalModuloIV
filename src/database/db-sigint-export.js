66921f2ea22b4a630ae210ebd6be9f96c29aa7a1
fabianobarroli <fabianobarroli@gmail.com>
2022-03-25 14:38:34 -0300

    verificando a identação do db-sigint-export

--- src/database/db-sigint-export.js
+++ src/database/db-sigint-export.js
@@ -1,12 +1,12 @@
 import sqlite3 from 'sqlite3'
-import {dirname} from 'path'
+import { dirname } from 'path'
 import { fileURLToPath } from 'url'
 sqlite3.verbose()
 const filePath = dirname(fileURLToPath(import.meta.url)) + '/bancoDeDadosMatricula.db'
 const bdm = new sqlite3.Database(filePath);
 
-process.on('SIGINT', ()=> 
-    bdm.close(()=>{
+process.on('SIGINT', () =>
+    bdm.close(() => {
         console.log('BDM fechado!');
         process.exit(0);
     })
