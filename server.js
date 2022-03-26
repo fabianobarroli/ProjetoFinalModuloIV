afb2dc09fc97fe47c4a73fecc4912a36227b859d
fabianobarroli <fabianobarroli@gmail.com>
2022-03-25 22:08:58 -0300

    inserindo o link do repositório

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import app from './app.js'
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-    res.send('<h1>Para maiores informaões acesse <a>https://github.com/fabianobarroli/ProjetoFinalModuloIV</a> </h1>')
+    res.send('<h1>Para maiores informaões acesse <a>https://github.com/fabianobarroli/ProjetoFinalModuloIV</a></h1>')
 })
 
 app.listen(port, () => {
